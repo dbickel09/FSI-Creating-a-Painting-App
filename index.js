f5c312551235cd9fd457e4bace2f9743ad2e3ae8
Dan <bickel09@yahoo.com>
2022-02-05 09:25:48 -0600

    Completed step 6

---
+++ index.js
@@ -0,0 +1,6 @@
+let selectedColor = 'blue'
+const painting = document.querySelector('.painting')
+
+painting.addEventListener('click', function(e){
+    e.target.style.backgroundColor = selectedColor
+})
